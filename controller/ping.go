40dbf98e773ed456480452d43329be5bb041d23e
daniel.zl <daniel.zl@meetsocial.com>
2022-04-12 21:48:42 +0800

    加入viper

--- controller/ping.go
+++ controller/ping.go
@@ -6,5 +6,4 @@ func Ping(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
-
 }
--- main.go
+++ main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"github.com/zlongCoding/excelCrm/controller"
 )
 
 func main() {
+	InitConfig()
 	r := gin.Default()
+
 	// r.Get("/ping", )
 	r.GET("/ping", controller.Ping)
-	err := r.Run()
-	if err != nil {
-		panic(err)
-	}
 
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
+	panic(r.Run())
+}
+
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("")
+	}
 }
